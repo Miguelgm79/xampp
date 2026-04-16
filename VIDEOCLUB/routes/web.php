2d4cb875962e07dd1f73c095f543c22c178b131d
Miguelgm79 <miguelgm79@educastur.es>
2026-04-16 11:01:56 +0200

    Laravel-01

--- VIDEOCLUB/routes/web.php
+++ VIDEOCLUB/routes/web.php
@@ -3,5 +3,30 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('login', function () {
+    return view('auth.login');
+});
+
+Route::get('logout', function () {
+    return 'Logout usuario';
+});
+
+Route::get('catalog', function () {
+    return view('catalog.index');
+});
+
+Route::get('catalog/show/{id?}', function ($id = 0) {
+    return view('catalog.show', array('id'=>$id));
+});
+
+Route::get('catalog/create', function () {
+    return view('catalog.create');
+});
+
+Route::get('catalog/edit/{id?}', function ($id = 0) {
+    return view('catalog.edit');
+})
+->where('id', '[0-9]+');
